720f299730152f634eaafa53adfb520c6f8dc680
Keks <keks@htmlacademy.ru>
2022-07-03 18:52:43 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,7 @@
+import {renderingThumbnails} from './render-template.js';
+import {arrayPhotoObject} from './data.js';
+import './work-with-form.js';
+import './edit-photo.js';
+
+renderingThumbnails(arrayPhotoObject);
+
