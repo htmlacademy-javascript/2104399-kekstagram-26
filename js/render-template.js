d12be935d94814b2a63ddc97cfe6e08d8e06872b
Sergey Lizunov <s.lizunov2020@yandex.ru>
2022-07-18 20:58:05 +0300

    Внесение правок перед проверкой

--- js/render-template.js
+++ js/render-template.js
@@ -1,6 +1,6 @@
 import {showBigPhoto} from './show-photo.js';
 
-const renderingThumbnails = (arrayPhotoObject) => {
+const renderingThumbnails = (photos) => {
   // Находим контейнер для изображений, куда будем помещать фото.
   const similarPhotoElement = document.querySelector('.pictures');
 
@@ -11,7 +11,7 @@ const renderingThumbnails = (arrayPhotoObject) => {
   const similarPhotoFragment = document.createDocumentFragment();
 
   // Цикл по массиву.
-  arrayPhotoObject.forEach((photo) => {
+  photos.forEach((photo) => {
     const photoElement = templateElement.cloneNode(true);
     photoElement.querySelector('.picture__img').src = photo.url;
     photoElement.querySelector('.picture__likes').textContent = photo.likes;
